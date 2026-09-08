1ce6d8641b77b62040a0b7bb7b9bb7174fd9f555
Vince <vinceemejas24@gmail.com>
2026-09-08 11:53:43 +0800

    ui changes

--- alertroad-frontend/src/utils/riskColors.js
+++ alertroad-frontend/src/utils/riskColors.js
@@ -1,4 +1,4 @@
-export const RISK_COLORS = { High: "#7a1c1c", Medium: "#d3ad04", Low: "#098d2e" };
+export const RISK_COLORS = { High: "#b3261e", Medium: "#b07c00", Low: "#1e7b4d" };
 export function getRiskColor(riskLevel) {
   return RISK_COLORS[riskLevel] || RISK_COLORS.Low;
 }
